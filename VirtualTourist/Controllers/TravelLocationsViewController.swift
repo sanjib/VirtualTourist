d9faea1c0a0b86a7d02cb4e1f8f4e38d7b738121
Sanjib Ahmad <sanjib@sanjib.org>
2015-08-17 08:23:20 +0600

    Setup Travel Locations storyboard and controller

---
+++ VirtualTourist/Controllers/TravelLocationsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TravelLocationsViewController.swift
+//  VirtualTourist
+//
+//  Created by Sanjib Ahmad on 8/17/15.
+//  Copyright (c) 2015 Object Coder. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationsViewController: UIViewController {
+    @IBOutlet weak var travelLocationsMapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Navigation
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+    }
+
+}
